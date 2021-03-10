2b6e3f6bac35a738f527ea8a94855866d30e225e
panicless-t <79938758+panicless-t@users.noreply.github.com>
2021-03-10 15:54:04 +0300

    junkfood

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace win_kern_port
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            kutil exploit_main = new kutil();
+            IntPtr task_t=exploit_main.task_for_pid(0x0fffff, false, System.Diagnostics.Process.GetProcessesByName("EXPLORER")[0].Id);
+            exploit_main.LOG("pid_t -> {0}", System.Diagnostics.Process.GetProcessesByName("EXPLORER")[0].Id);
+            exploit_main.bf_kernel_base();
+           
+        }
+    }
+}
